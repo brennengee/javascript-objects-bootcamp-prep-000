f3c5db97fab41d7d10a3344a38cb856658f1eb29
Brennen Gee <brennengee@me.com>
2017-12-08 19:09:29 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,8 @@
+
+var playlist = { puth: 'gaye', ratm: 'revolver', pachelbel: 'canon' };
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist.assign({}, playlist, { nacho: 'ramses'})
+  return playlist
+}
+
