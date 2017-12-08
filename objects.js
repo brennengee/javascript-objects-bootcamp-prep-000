c2c3bf22a49e8438265c645d2cce24d226e5df9f
Brennen Gee <brennengee@me.com>
2017-12-08 20:04:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,3 +9,5 @@ function removeFromPlaylist(playlist, puth) {
   delete playlist[puth];
   return playlist
 }
+
+////OKAY I wasted way too much time on this. Especially the 5 minutes where I had a period instead of the brackets, because the tutorial showed a period. Why would they do that? Thank goodness for StackOverflow. 
