e189732c0f7b5ddf26c9d392c42b7a8b7c88aa9d
Brennen Gee <brennengee@me.com>
2017-12-08 19:38:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 
 var playlist = new Object({ puth: 'gaye', ratm: 'revolver', pachelbel: 'canon' });
 
-function updatePlaylist(playlist) {
+function updatePlaylist(playlist, nacho, ramses) {
   Object.assign({}, playlist, { nacho: 'ramses'})
   return playlist
 }
